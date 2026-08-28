3a4d34379d10f031deaee18af844c33f631b554f
RHZ76 <rene@viacruz.com>
2026-08-28 11:09:57 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='zeitzwerk-v1-1-2h';
-const F=['./','index.html','style.css?v=1.1.2h','app.js?v=1.1.2h','manifest.webmanifest','icon.svg'];
+const C='zeitzwerk-v1-1-2i';
+const F=['./','index.html','style.css?v=1.1.2i','app.js?v=1.1.2i','manifest.webmanifest','icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(F)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).catch(()=>caches.match('./')));return}e.respondWith(fetch(e.request).then(r=>{let copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
